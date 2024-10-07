3045fbd01881d884fb30ee2e6c38b598902c91e3
Facundo VARAS <102789450+cundovar@users.noreply.github.com>
2024-10-07 17:44:50 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+  <script setup>
+  import {ref} from 'vue'
+import Apibanque from './components/api/Apibanque.vue';
+
+
+  const nom="john"
+  const count=ref(0)
+  const increment=ref(0)
+  
+  const incrementaion=()=>{
+    increment.value++
+  }
+  setInterval(() => {
+    if(count.value >=10){
+      count.value=0
+    
+    }else{
+      count.value++
+  
+    }
+  },1000);
+  </script>
+
+
+
+<template>
+<h1>bonjour <span class="text-pink-600">{{ nom.toUpperCase() }} </span> </h1>
+<p>comment aller vous ?</p>
+<p>comteur = {{ count }}</p>
+<div class="space-x-5 flex">
+
+  <button class="border p-2 focus:text-purple-500 text-2xl" v-on:click="incrementaion" >+  </button> <span class="border rounded-full w-10 h-10 flex justify-center items-center bg-violet-300 p-3 "> 
+    <div class="relative  h-4 w-2" >
+      <p class="absolute bottom-0 m-0 p-0"> {{ increment }}</p> 
+
+    </div>
+
+</span> <button type="button" class="text-2xl  border p-2" @click="increment--">-</button>
+</div>
+
+<Apibanque/>
+
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/api/Apibanque.vue
@@ -0,0 +1,58 @@
+
+
+<template>
+ <h1>
+    get name peint
+</h1>
+    <ul>
+       
+        <li class="flex space-x-6" v-for="peintItem in peint" :key="peintItem.id">
+          nom :   <p class="text-black">
+              
+              {{peintItem.name}}
+          </p>
+          <img :class="m-auto " :src="'https://bac.varascundo.com/images/oeuvre/'+peintItem.image " class="w-10 h-10 "/>
+        </li>
+    </ul>
+
+
+
+</template>
+
+
+
+    <script>
+
+    import axios from 'axios';
+
+    export default {
+  name: 'Apibanque',
+  data(){
+    return{
+        peint:[],
+       
+    }
+  },
+  mounted(){
+    this.getPeint()
+    
+  },
+  methods:{
+    async getPeint(){
+        try{
+            
+            const response=await axios.get("https://bac.varascundo.com/api/oeuvres/")
+            this.peint=response.data['hydra:member']
+            console.log(this.peint)
+         
+        }catch(error){
+            console.error("erreur récup",error)
+        }
+    }
+   
+  }
+};
+
+    
+    
+    </script>
